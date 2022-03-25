7d9d3bbc07e25183af21bdfa30145a41517232de
Fargol Shirvanifar <fargolsh79@gmail.com>
2022-03-25 22:36:35 +0430

    clearing and adding items - more bug fixings

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 // VARIABLES
-let changeMode = document.querySelector(".header-icon");
-let body = document.querySelector("body");
-let header = document.querySelector(".header");
-let todoNew = document.querySelector(".todo__new");
-let todoNewCheckboxButton = document.querySelector(".todo__new--checkbox-button");
-let todoNewInput = document.querySelector(".todo__new--input");
-let todoList = document.querySelector(".todo__list");
-let todoListCheckboxButtons = document.querySelectorAll(".todo__list--checkbox-button");
-let todoListItems = document.querySelectorAll(".todo__list--item");
-let todoListLabels = document.querySelectorAll(".todo__list--checkbox-labelText");
-let todoListInputs = document.querySelectorAll(".todo__list--checkbox-input");
-let todoStatusItems = document.querySelectorAll('.todo__list--footer-statusItems');
-let todoClear = document.querySelector('.todo__list--footer-clearText');
+let changeMode = document.getElementsByClassName("header-icon")[0];
+let body = document.getElementsByTagName("body")[0];
+let header = document.getElementsByClassName("header")[0];
+let todoNew = document.getElementsByClassName("todo__new")[0];
+let todoNewCheckboxButton = document.getElementsByClassName("todo__new--checkbox-button")[0];
+let todoNewInput = document.getElementsByClassName("todo__new--input")[0];
+let todoList = document.getElementsByClassName("todo__list")[0];
+let todoListCheckboxButtons = document.getElementsByClassName("todo__list--checkbox-button");
+let todoListItems = document.getElementsByClassName("todo__list--item");
+let todoListLabels = document.getElementsByClassName("todo__list--checkbox-labelText");
+let todoListInputs = document.getElementsByClassName("todo__list--checkbox-input");
+let todoStatusItems = document.getElementsByClassName('todo__list--footer-statusItems');
+let todoClear = document.getElementsByClassName('todo__list--footer-clearText')[0];
 let sunIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="26" height="26"><path fill="#FFF" fill-rule="evenodd" d="M13 21a1 1 0 011 1v3a1 1 0 11-2 0v-3a1 1 0 011-1zm-5.657-2.343a1 1 0 010 1.414l-2.121 2.121a1 1 0 01-1.414-1.414l2.12-2.121a1 1 0 011.415 0zm12.728 0l2.121 2.121a1 1 0 01-1.414 1.414l-2.121-2.12a1 1 0 011.414-1.415zM13 8a5 5 0 110 10 5 5 0 010-10zm12 4a1 1 0 110 2h-3a1 1 0 110-2h3zM4 12a1 1 0 110 2H1a1 1 0 110-2h3zm18.192-8.192a1 1 0 010 1.414l-2.12 2.121a1 1 0 01-1.415-1.414l2.121-2.121a1 1 0 011.414 0zm-16.97 0l2.121 2.12A1 1 0 015.93 7.344L3.808 5.222a1 1 0 011.414-1.414zM13 0a1 1 0 011 1v3a1 1 0 11-2 0V1a1 1 0 011-1z"/></svg>';
 let moonIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="26" height="26"><path fill="#FFF" fill-rule="evenodd" d="M13 0c.81 0 1.603.074 2.373.216C10.593 1.199 7 5.43 7 10.5 7 16.299 11.701 21 17.5 21c2.996 0 5.7-1.255 7.613-3.268C23.22 22.572 18.51 26 13 26 5.82 26 0 20.18 0 13S5.82 0 13 0z"/></svg>';
 let mode = 0; // light mode by default
@@ -109,21 +109,11 @@ const statusHandler = function(i) {
 }
 
 const clearHandler = function() {
-  let newElements = [];
-  let todoLength = todoListItems.length;
-  for(let i = 0; i < todoLength; i++) {
+  for(let i = 0; i < todoListItems.length; i++) {
     if(todoListInputs[i].checked === true) {
-      // for(let k = 0; k < todoListItems.length; k++) {
-      //   if(k !== i) {
-      //       newElements.push(todoListItems[i]);
-      //   }
-      // }
-      // todoListItems = newElements;
-      // newElements = [];
       todoList.removeChild(todoListItems[i]);
     }
   }
-
 }
 // EVENT LISTENRES
 // change mode
@@ -176,6 +166,7 @@ todoClear.addEventListener('mouseleave', (event) => {
 for(let i = 0; i < todoListItems.length; i++) {
   todoListItems[i].addEventListener('mouseenter', (event) => {
     if(mode == 0) {
+      console.log(i);
       todoListCheckboxButtons[i].style.background = "linear-gradient(#fff, #fff) padding-box, linear-gradient(to right bottom, hsl(192, 100%, 67%), hsl(280, 87%, 65%)) border-box";
     } else {
       todoListCheckboxButtons[i].style.background = "linear-gradient(#25273c, #25273c) padding-box, linear-gradient(to right bottom, hsl(192, 100%, 67%), hsl(280, 87%, 65%)) border-box";
