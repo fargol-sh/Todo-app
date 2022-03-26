950fa65b9e9e3294638ad5831219ff75385daf01
Fargol Shirvanifar <fargolsh79@gmail.com>
2022-03-27 00:45:26 +0430

    clear and cross icon handler

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ let todoStatusItems = document.getElementsByClassName('todo__list--footer-status
 let todoClear = document.getElementsByClassName('todo__list--footer-clearText')[0];
 let sunIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="26" height="26"><path fill="#FFF" fill-rule="evenodd" d="M13 21a1 1 0 011 1v3a1 1 0 11-2 0v-3a1 1 0 011-1zm-5.657-2.343a1 1 0 010 1.414l-2.121 2.121a1 1 0 01-1.414-1.414l2.12-2.121a1 1 0 011.415 0zm12.728 0l2.121 2.121a1 1 0 01-1.414 1.414l-2.121-2.12a1 1 0 011.414-1.415zM13 8a5 5 0 110 10 5 5 0 010-10zm12 4a1 1 0 110 2h-3a1 1 0 110-2h3zM4 12a1 1 0 110 2H1a1 1 0 110-2h3zm18.192-8.192a1 1 0 010 1.414l-2.12 2.121a1 1 0 01-1.415-1.414l2.121-2.121a1 1 0 011.414 0zm-16.97 0l2.121 2.12A1 1 0 015.93 7.344L3.808 5.222a1 1 0 011.414-1.414zM13 0a1 1 0 011 1v3a1 1 0 11-2 0V1a1 1 0 011-1z"/></svg>';
 let moonIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="26" height="26"><path fill="#FFF" fill-rule="evenodd" d="M13 0c.81 0 1.603.074 2.373.216C10.593 1.199 7 5.43 7 10.5 7 16.299 11.701 21 17.5 21c2.996 0 5.7-1.255 7.613-3.268C23.22 22.572 18.51 26 13 26 5.82 26 0 20.18 0 13S5.82 0 13 0z"/></svg>';
+let crossIcons = document.getElementsByClassName('todo__list--item-crossIcon');
 let mode = 0; // light mode by default
 let status = 0; // 0 : All , 1 : Active, 2 : Completed
 
@@ -112,8 +113,13 @@ const clearHandler = function() {
   for(let i = 0; i < todoListItems.length; i++) {
     if(todoListInputs[i].checked === true) {
       todoList.removeChild(todoListItems[i]);
+      i--;
     }
   }
+}
+
+const crossIconHandler = function(i) {
+  todoList.removeChild(todoListItems[i]);
 }
 // EVENT LISTENRES
 // change mode
@@ -196,7 +202,9 @@ for(let i = 0; i < todoStatusItems.length; i++) {
   todoStatusItems[i].addEventListener('click', statusHandler.bind(this, i));
 }
 
-// todo list clear
+// clear todo list
 todoClear.addEventListener('click', clearHandler);
-
+for(let i = 0; i < todoListItems.length; i++) {
+  crossIcons[i].addEventListener('click', crossIconHandler.bind(this, i));
+}
 ////
